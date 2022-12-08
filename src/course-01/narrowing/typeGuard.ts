f7ddb6b43ff0ecedbd9898aea78822dbd70cdd41
unknown <contato.soer@gmail.com>
2022-12-08 20:18:03 -0300

    New Section - Narrowing

---
+++ src/course-01/narrowing/typeGuard.ts
@@ -0,0 +1,14 @@
+// Type Guard
+function sum(valueOne: number | string, valueTwo: number | string) {
+  if (typeof valueOne === 'string' && typeof valueTwo === 'string') {
+    console.log(Number(valueOne) + Number(valueTwo));
+  } else if (typeof valueOne === 'number' && typeof valueTwo === 'number') {
+    console.log(valueOne + valueTwo);
+  } else {
+    console.log('Insert two strings or two numbers...');
+  }
+}
+
+sum('1', '4');
+sum(5, 5);
+sum('5', 5);
